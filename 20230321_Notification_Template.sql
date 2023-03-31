8515bdc140dd546fc57ff2dc69e8401883d8110a
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-03-31 15:19:47 +0700

    update Notification Template

--- 20230321_Notification_Template.sql
+++ 20230321_Notification_Template.sql
@@ -1,4 +1,5 @@
 UPDATE NOTIFICATION_MANAGEMENT_SERVICE.NOTIFICATION_TEMPLATE
-SET ICON = 'http://10.0.116.118:9000/bsi/assets/notification-banner/top-up-griya-approved-rebrand-2x.png'
+SET MESSAGE_TITLE = 'Status pengajuan top up griya online kamu telah disetujui',
+    ICON = 'http://10.0.116.118:9000/bsi/assets/notification-banner/top-up-griya-approved-rebrand-2x.png'
 WHERE TEMPLATE_TYPE = 'INBOX'
     AND TEMPLATE_CODE = 'FINTopUpGriya_APPROVED';
