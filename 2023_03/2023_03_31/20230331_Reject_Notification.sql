56a0b5a06ad39ab90c98a27b026ffcb517ee6471
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-04-05 09:24:18 +0700

    update Reject Notification

--- 2023_03/2023_03_31/20230331_Reject_Notification.sql
+++ 2023_03/2023_03_31/20230331_Reject_Notification.sql
@@ -8,12 +8,13 @@ UPDATE NOTIFICATION_MANAGEMENT_SERVICE.NOTIFICATION_TEMPLATE
 SET MESSAGE_TITLE = 'Status pengajuan top up griya online kamu belum disetujui.',
     ICON = 'http://10.0.116.118:9000/bsi/assets/notification-banner/reject-notification-2x.png',
     MESSAGE_CONTENT = TO_CLOB('Assalamualaikum ${name},
-<br>
+<br/><br/>
 Mohon maaf dengan ini kami menyampaikan bahwa status pengajuan kamu <strong>belum dapat kami setujui</strong>. 
-<br>
+<br/><br/>
 Untuk informasi pembiayaan lebih lanjut dan fasilitas lainnya silahkan chat Aisyah atau dapat menghubungi Call Center BSI di 14040
-<br>
-<br>
+<br/>
+<br/>
+<br/>
 Terima kasih untuk tetap setia menggunakan BSI')
 WHERE TEMPLATE_TYPE = 'INBOX'
     AND TEMPLATE_CODE = 'FINTopUpGriya_REJECTED';
